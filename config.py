4b5a63211c0407116950c77339b5a734c1ade2e3
Laurent <laurent@gilgamesh>
2024-10-08 07:49:24 +0200

    Chore:  clean up comments

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ INIT_INDEX = os.getenv('INIT_INDEX', 'false').lower() == 'true'
 # vector index persist directory
 INDEX_PERSIST_DIRECTORY = os.getenv('INDEX_PERSIST_DIRECTORY', "./data/chromadb")
 
-# target url to scrape
+# URL of the site to scrape
 TARGET_URL =  os.getenv('TARGET_URL', "https://tm-en.doc.squashtest.com/latest/")
 
-# http api port
+# HTTP API port
 HTTP_PORT = os.getenv('HTTP_PORT', 7654)

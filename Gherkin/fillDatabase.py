a943bfc3afa342e443ed9e728210b9042eda0b32
Laurent <laurent@gilgamesh>
2024-10-12 21:59:08 +0200

    Feat: record the keywords in ChromaDB databases

--- Gherkin/fillDatabase.py
+++ Gherkin/fillDatabase.py
@@ -1,20 +1,40 @@
 import chromadb
+import json
+import sys
+import pprint
 
 database_location = "./chromadb/database"
 
 chroma_client = chromadb.PersistentClient(path=database_location)
 
-collection = chroma_client.get_or_create_collection(name="my_collection")
 
-collection.upsert(
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about oranges"
-    ],
-    ids=["id1", "id2"]
-)
-results = collection.query(
-    query_texts=["This is a query document about florida"], # Chroma will embed this for you
-    n_results=2 # how many results to return
-)
-print(results)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py keyword_list.json")
+        sys.exit(1)
+    keyword_file = sys.argv[1]
+
+    collections = {
+        "Context": chroma_client.get_or_create_collection(name="Context"),
+        "Action": chroma_client.get_or_create_collection(name="Action"),
+        "Outcome": chroma_client.get_or_create_collection(name="Outcome")
+    }
+
+    with open(keyword_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    keywords = data['keywords']
+
+    incr = 0
+    for item in keywords:
+          collections[item['type']].upsert(documents=[item['keyword']], ids=[f"id{incr:03d}"])
+          incr += 1
+ 
+    results = collections["Outcome"].query(
+        query_texts=["I am on the command page"], # Chroma will embed this for you
+        n_results=3 # how many results to return
+    )
+    pprint.pp(results, width=128)
+
+if __name__ == "__main__":
+    main()

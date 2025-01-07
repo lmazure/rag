7cf4db8544e1c006920e9ac9566b917a72508491
Laurent <laurent@gilgamesh>
2025-01-07 23:01:41 +0100

    Feat: improve search

--- Gherkin/common.py
+++ Gherkin/common.py
@@ -1,4 +1,10 @@
 import re
+import chromadb
+from chromadb.config import Settings
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+import common
+
+### management of collection names
 
 def get_keyword_type(collection_name: str) -> str:
     """
@@ -32,6 +38,10 @@ def get_collection_name(model_name: str, project_name: str, keyword_type: str) -
     if keyword_type not in ["Context", "Action", "Outcome"]:
         raise ValueError("Error: Keyword type can only be 'Context', 'Action', or 'Outcome'.")
     return f"{model_name}-{project_name}-{keyword_type}"
+
+
+
+#### management of ids
 
 def get_document_type(internal_id: str) -> str:
     """
@@ -79,3 +89,86 @@ def get_internal_id_of_description(internal_keyword_id: str) -> str:
     if get_document_type(internal_keyword_id) != 'keyword':
         raise ValueError("Not id of a keyword")
     return f"{get_external_id(internal_keyword_id)}-d"
+
+def get_internal_id_of_keyword(internal_description_id: str) -> str:
+    """
+    Get the internal id of a keyword given the internal id of its description.
+    
+    Args:
+        internal_description_id: The internal id of the description.
+        
+    Returns:
+        The internal id of the keyword.
+        
+    Raises:
+        ValueError: If the provided id is not of a description.
+    """
+    if get_document_type(internal_description_id) != 'description':
+        raise ValueError("Not id of a description")
+    return f"{get_external_id(internal_description_id)}-k"
+
+
+### database queries
+
+def extract_keywords(db_path: str, model: str, project: str, keyword_type: str, keyword: str, nb_results:int) -> list[dict[str, str]]:
+    """
+    Extract the nearest neighbours of a keyword from a Chroma database.
+
+    Args:
+        db_path: The path to the Chroma database.
+        model: The name of the model to use for the extraction.
+        project: The name of the project to use for the extraction.
+        keyword_type: The type of keyword to extract.
+        keyword: The keyword to search for.
+        nb_results: The number of results to return.
+
+    Returns:
+        A list of dictionaries with the following keys:
+            - id: The external id of the matching keyword.
+            - match: What is matching the searched keyword ('keyword' or 'description').
+            - keyword: The text of the matching keyword.
+            - description: The description of the matching keyword. Absent if the keyword has no description.
+            - keyword_distance: (May be missing if description matches) the distance between the matching keyword and the searched keyword.
+            - description_distance: (May be missing if keyword matches) the distance between the description of the matching keyword and the searched keyword.
+
+    Raises:
+        ValueError: If the model and/or project do not exist in the database.
+    """
+    # Initialize Chroma client
+    chroma_client = chromadb.PersistentClient(path=db_path, settings=Settings(anonymized_telemetry=False))
+
+    # Get the appropriate collection
+    collection_name = f"{common.get_collection_name(model, project, keyword_type)}"
+    try:
+        collection = chroma_client.get_collection(
+            name=collection_name,
+            embedding_function=SentenceTransformerEmbeddingFunction(model_name=model)
+        )
+    except chromadb.errors.InvalidCollectionException as e:
+        raise ValueError(f"Error: Model {model} and/or project {project} do not exist in database {db_path}.") from e
+
+    documents = collection.get(include=['documents'])
+
+    # Perform the search query
+    search_results = collection.query(
+        query_texts=[keyword],
+        n_results=nb_results
+    )
+
+    data = []
+    for i in range(len(search_results['ids'][0])):
+        if common.get_document_type(search_results['ids'][0][i]) == 'keyword':
+            d = { 'id': common.get_external_id(search_results['ids'][0][i]), 'match': 'keyword', 'keyword': search_results['documents'][0][i], 'keyword_distance': search_results['distances'][0][i]}
+            description_id = common.get_internal_id_of_description(search_results['ids'][0][i])
+            if description_id in documents['ids']:
+                d['description'] = documents['documents'][documents['ids'].index(description_id)]
+                if description_id in search_results['ids'][0]:
+                    d['description_distance'] = search_results['distances'][0][search_results['ids'][0].index(description_id)]
+        else:
+            d = { 'id': common.get_external_id(search_results['ids'][0][i]), 'match': 'description', 'description': search_results['documents'][0][i], 'description_distance': search_results['distances'][0][i]}
+            keyword_id = common.get_internal_id_of_keyword(search_results['ids'][0][i])
+            d['keyword'] = documents['documents'][documents['ids'].index(keyword_id)]
+            if keyword_id in search_results['ids'][0]:
+                d['keyword_distance'] = search_results['distances'][0][search_results['ids'][0].index(keyword_id)]
+        data.append(d)
+    return data
--- Gherkin/query_database.py
+++ Gherkin/query_database.py
@@ -1,50 +1,26 @@
 import argparse
-import chromadb
 from chromadb.config import Settings
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 import common
-import sys
-
-def extract_keywords(db_path: str, model: str, project: str, keyword_type: str, keyword: str, nb_results:int) -> list[dict[str, str]]:
-    # Initialize Chroma client
-    chroma_client = chromadb.PersistentClient(path=db_path, settings=Settings(anonymized_telemetry=False))
-
-    # Get the appropriate collection
-    collection_name = f"{common.get_collection_name(model, project, keyword_type)}"
-    try:
-        collection = chroma_client.get_collection(
-            name=collection_name,
-            embedding_function=SentenceTransformerEmbeddingFunction(model_name=model)
-        )
-    except chromadb.errors.InvalidCollectionException as e:
-        print(f"Error: Model {model} and/or project {project} do not exist in database {db_path}.", file=sys.stderr)
-        sys.exit(1)
-
-    # Perform the query
-    results = collection.query(
-        query_texts=[keyword],
-        n_results=nb_results
-    )
-
-    return [{"id": results['ids'][0][i][:-2], "keyword": results['documents'][0][i], "distance": results['distances'][0][i]} for i in range(len(results['ids'][0]))]
 
 def main():
     parser = argparse.ArgumentParser(description="Query Chroma database for keyword matches.")
     parser.add_argument("--model", default="all-MiniLM-L6-v2", help="Embedding model to use (default: all-MiniLM-L6-v2)")
     parser.add_argument("--db_path", default="./chromadb/database", help="Path to the Chroma database (default: ./chromadb/database)")
     parser.add_argument("--project", default="Common", help="Name of the project (default: Common)")
-    parser.add_argument("--nb_results", default=3, type=int, required=True, help="Number of matches to return (default: 3)")
+    parser.add_argument("--nb_results", default=3, type=int, help="Number of matches to return (default: 3)")
     parser.add_argument("--keyword_type", required=True, choices=["Context", "Action", "Outcome"], help="Type of keyword")
     parser.add_argument("keyword", help="Keyword to query")
     
     args = parser.parse_args()
 
-    results = extract_keywords(args.db_path, args.model, args.project, args.keyword_type, args.keyword, args.nb_results)
+    results = common.extract_keywords(args.db_path, args.model, args.project, args.keyword_type, args.keyword, args.nb_results)
 
     # Print results
-    print(f"Top {len(results)} matches for '{args.keyword}' in {args.keyword_type} category:")
+    print(f"Top {len(results)} matches for '{args.keyword}' in {args.project} project in {args.keyword_type} category:")
+    print("ID\tMatch\tKeyword\tKeyword distance\tDescription\tDescription distance")
     for result in results:
-        print(f"{result['id']}\t{result['keyword']}\t{result['distance']:.2f}")
+        print(f"{result['id']}\t{result['match']}\t{result['keyword']}\t{(result['keyword_distance']) if 'keyword_distance' in result else '-'}\t{result['description'] if 'description' in result else '-'}\t{(result['description_distance']) if 'description_distance' in result else '-'}")
 
 if __name__ == "__main__":
     main()
